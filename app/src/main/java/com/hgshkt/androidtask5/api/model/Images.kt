cd8486d9b8af10a3ec4a73e889810098c3e4d884
hgshkt <nichtojestvozxc@gmail.com>
2024-01-30 16:05:14 +0200

    class images added. super hero map property replaced to Images class

---
+++ app/src/main/java/com/hgshkt/androidtask5/api/model/Images.kt
@@ -0,0 +1,8 @@
+package com.hgshkt.androidtask5.api.model
+
+data class Images(
+    val xs: String,
+    val sm: String,
+    val md: String,
+    val lg: String
+)
--- app/src/main/java/com/hgshkt/androidtask5/api/model/SuperHero.kt
+++ app/src/main/java/com/hgshkt/androidtask5/api/model/SuperHero.kt
@@ -1,6 +1,6 @@
 package com.hgshkt.androidtask5.api.model
 
 data class SuperHero(
-    val name: String,
-    val images: Map<String, String>
+    val name: String?,
+    val images: Images
 )
