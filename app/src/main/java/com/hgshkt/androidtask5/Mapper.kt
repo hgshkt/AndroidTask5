d44c9dff86510637505eec048cf07ae5c2be2ad8
hgshkt <nichtojestvozxc@gmail.com>
2024-01-30 16:05:43 +0200

    mapper changed by images property

--- app/src/main/java/com/hgshkt/androidtask5/Mapper.kt
+++ app/src/main/java/com/hgshkt/androidtask5/Mapper.kt
@@ -1,20 +1,28 @@
 package com.hgshkt.androidtask5
 
+import com.hgshkt.androidtask5.api.model.Images
 import com.hgshkt.androidtask5.api.model.SuperHero
 import com.hgshkt.androidtask5.model.SuperHeroDisplay
-
-private const val emptyImageUrl = ""
 
 fun SuperHero.toDisplay(imageSizeType: ImageSizeType): SuperHeroDisplay {
     return SuperHeroDisplay(
         name = name,
-        imageUrl = images[imageSizeType.sizeName] ?: emptyImageUrl
+        imageUrl = urlByImageSize(imageSizeType, images)
     )
 }
 
-enum class ImageSizeType(val sizeName: String) {
-    XS("xs"),
-    SM("sm"),
-    MD("md"),
-    LG("lg"),
+private fun urlByImageSize(imageSizeType: ImageSizeType, images: Images): String {
+    return when(imageSizeType) {
+        ImageSizeType.XS -> images.xs
+        ImageSizeType.SM -> images.sm
+        ImageSizeType.MD -> images.md
+        ImageSizeType.LG -> images.lg
+    }
+}
+
+enum class ImageSizeType {
+    XS,
+    SM,
+    MD,
+    LG,
 }
