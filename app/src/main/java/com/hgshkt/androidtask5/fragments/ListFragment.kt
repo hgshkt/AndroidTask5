17432e30cb0caa42e77db1d0137f568a21afee6e
hgshkt <nichtojestvozxc@gmail.com>
2024-01-31 15:37:25 +0200

    details and list fragments classes

---
+++ app/src/main/java/com/hgshkt/androidtask5/fragments/ListFragment.kt
@@ -0,0 +1,18 @@
+package com.hgshkt.androidtask5.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ListFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
