6644038a45eaabcab07f78c7a1bf97fec30df335
hgshkt <nichtojestvozxc@gmail.com>
2024-02-03 17:39:34 +0200

    SuperHero powerStats param marked as @SerializedName("powerstats")

--- app/src/main/java/com/hgshkt/androidtask5/api/model/SuperHero.kt
+++ app/src/main/java/com/hgshkt/androidtask5/api/model/SuperHero.kt
@@ -1,8 +1,11 @@
 package com.hgshkt.androidtask5.api.model
 
+import com.google.gson.annotations.SerializedName
+
 data class SuperHero(
     val name: String,
     val images: Images,
+    @SerializedName("powerstats")
     val powerStats: PowerStats?,
     val biography: Biography?
 )
