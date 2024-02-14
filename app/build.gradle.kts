1827f54d2ffd36c60c340797ef51bbb450e2d56c
hgshkt <nichtojestvozxc@gmail.com>
2024-02-14 22:41:11 +0200

    coroutines dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,9 @@ dependencies {
     // Glide
     implementation("com.github.bumptech.glide:glide:4.15.1")
 
+    // Coroutines
+    implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-experimental-adapter:1.0.0")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
